86e2ea2624d70959fb2e33f30896137af345e7dd
victor kaua <dev.victorkaua@gmai.com>
2026-03-23 18:37:33 -0300

    very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,55 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("room number: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Check-in date (dd/MM/yyyy): ");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.println("Check-Out date (dd/MM/yyyy");
+        Date checkOut = sdf.parse(sc.next());
+
+        if
+        (!checkOut.after(checkIn)) {
+            System.out.println("Error in reservation: Check-out date must be after check-in date");
+        } else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.println("CheckIn Date (dd/MM/yyyy): ");
+            checkIn = sdf.parse(sc.next());
+            System.out.println("CheckOut Date (dd/MM/yyyy): ");
+            checkOut = sdf.parse(sc.next());
+            Date now = new Date();
+            if (checkIn.before(now) || checkOut.before(now)) {
+                System.out.println("Error in reservation date for update");
+            } else if (!checkOut.after(checkIn)) {
+                System.out.println("Error in reservation date for update");
+
+            }else{
+             reservation.upDates(checkIn,checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+
+
+        }
+
+
+        sc.close();
+
+    }
+}
